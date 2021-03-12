9ec2e3dac9cffe86bd9ff34236e2e7e435e954a6
talasilwadi116 <talajamal17@gmail.com>
2021-03-12 02:24:03 +0200

    the project will work on MVC . Model include person , member , group , group admin , activity to the right now ! View : Login , register & main activity UI Controller : Logincontroller , registercontoller,mainactivitController .
    Connected to firebase to jam3na.application17@gmail.com Email & testing register .
    the UI not finish !

---
+++ app/src/androidTest/java/Model/Member.java
@@ -0,0 +1,56 @@
+package Model;
+
+public class Member extends Person{
+
+    public double Age , Weight , height , Rate ;
+    public char BloodType ;
+
+    public Member(double age, double weight, double height, double rate, char bloodType) {
+        super();
+        Age = age;
+        Weight = weight;
+        this.height = height;
+        Rate = rate;
+        BloodType = bloodType;
+    }
+
+    public double getAge() {
+        return Age;
+    }
+
+    public void setAge(double age) {
+        Age = age;
+    }
+
+    public double getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(double weight) {
+        Weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getRate() {
+        return Rate;
+    }
+
+    public void setRate(double rate) {
+        Rate = rate;
+    }
+
+    public char getBloodType() {
+        return BloodType;
+    }
+
+    public void setBloodType(char bloodType) {
+        BloodType = bloodType;
+    }
+}
---
+++ app/src/androidTest/java/Model/Person.java
@@ -0,0 +1,70 @@
+package Model;
+
+public class Person {
+    public String UserFirstname  , UserLastName , Email , Password ;
+    public int PhoneNumber  ;
+
+    public Person() {
+
+    }
+    public Person(String userFirstname, String userLastName, String email, String password, int phoneNumber) {
+        UserFirstname = userFirstname;
+        UserLastName = userLastName;
+        Email = email;
+        Password = password;
+        PhoneNumber = phoneNumber;
+    }
+
+
+
+    public String getUserFirstname() {
+        return UserFirstname;
+    }
+
+    public void setUserFirstname(String userFirstname) {
+        UserFirstname = userFirstname;
+    }
+
+    public String getUserLastName() {
+        return UserLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        UserLastName = userLastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "UserFirstname='" + UserFirstname + '\'' +
+                ", UserLastName='" + UserLastName + '\'' +
+                ", Email='" + Email + '\'' +
+                ", Password='" + Password + '\'' +
+                ", PhoneNumber=" + PhoneNumber +
+                '}';
+    }
+}
